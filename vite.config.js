0cff3c17ed077bd0a8c3bb44a3b251baba413bee
Sissel <32619816+Siss3l@users.noreply.github.com>
2023-06-17 18:59:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { VitePWA } from "vite-plugin-pwa"
 import react from "@vitejs/plugin-react"
 import copy from "rollup-plugin-copy"
 import csp from "vite-plugin-csp"
+// import million from "million/compiler"
 
 
 export default defineConfig({
@@ -56,7 +57,8 @@ export default defineConfig({
           "'sha512-z4PhNX7vuL3xVChQ1m2AB9Yg5AULVxXcg/SpIdNs6c5H0NE8XYXysP+DGNKHfuwvY7kxvUdBeoGlODJ6+SfaPg=='", // Empty hash
           "'sha512-j6tSXa6Yx/Whp8ra7DfhmbyBA4iNAx/s1lhqcbsMDBJU4B6IIw4aHBGS060qkgsD5sOSNndJoxkNLA1r/pA+eg=='", // width dynamic
           "'sha512-5V+mbF/yWInmfBAKEzYB4X1Y4JTGx05EuyO4aG4DiEv6FiuW9aF5/dGaBfnOhXS/Tg9kTnwLPxEf6ilCG7p9aA=='", // .xterm-bg-1
-          "'sha512-AVUwX86yevxWTFF5S3pXmfyvu2psSDt4AVQnfwl2VbAH6oUAPCxqB5p9W7V6TTmL3NmUkoTjHgZZBYhAEOntxw=='"  // 8px on Firefox
+          "'sha512-AVUwX86yevxWTFF5S3pXmfyvu2psSDt4AVQnfwl2VbAH6oUAPCxqB5p9W7V6TTmL3NmUkoTjHgZZBYhAEOntxw=='", // 8px on Firefox
+          "'sha256-Ot28rRDpuBkn69l5TRxj02UcsadcuEWPGelNYsmzUkA='" // For some MacOS
         ],
         "worker-src": ["'self'"],
         "require-trusted-types-for": ["'script'"],
@@ -79,17 +81,16 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "node-fetch": "axios" // externalized for browser compatibility
+      "node-fetch": "axios" // Externalized for browser compatibility
     },
   },
   server: {
     headers: {
       "Access-Control-Allow-Origin": "https://sissel.github.io",
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-      // "Service-Worker-Allowed": "/"
+      "Cross-Origin-Opener-Policy": "same-origin" // "Service-Worker-Allowed": "/",
     },
-    hmr: false,
+    hmr: false, // One instance
     https: true
   }
 })
