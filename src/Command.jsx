555591e57f1e0d55233d0ae4640bc64ce19ea873
Sissel <32619816+Siss3l@users.noreply.github.com>
2025-06-13 14:53:44 +0200

    Update Command.jsx

--- src/Command.jsx
+++ src/Command.jsx
@@ -6,8 +6,7 @@ import i18next from "./I18n.jsx";
 const commands = {
     /**
      * This `object` stores different commands that can be used in the `Terminal` with `Pyodide`.
-     * You can assign variables with the same name as the commands below with `eval` function.
-     *
+     * We could assign variables with the same name as the commands below.
      * @default object
      */
     audio: {
@@ -18,14 +17,14 @@ const commands = {
     },
     cat: {
         f: (command) => {
-            const cat = command?.match(/^cat\s+(.+)$/) || ["cat", "."];
+            const cat = command?.match(/^cat\s+(.+)$/) || ["cat", "."]; // Nano
             if (self.pyodide) {
                 self.term.runPythonCode(`cat = __import__("pathlib").Path("${cat[1]}"); txt = cat.read_bytes() if cat.is_file() else "cat: No such file or directory"; txt`).then(null);
             } else {
                 self.term.write("\ncat: No such file or directory\r\n>>> ");
             }
         },
-        description: i18next?.t("help.cat")
+        description: () => i18next?.t("help.cat")
     },
     cd: {
         f: (command) => {
@@ -41,7 +40,7 @@ const commands = {
                 self.term.write("\ncd: No such file or directory\r\n>>> ");
             }
         },
-        description: i18next?.t("help.cd")
+        description: () => i18next?.t("help.cd")
     },
     clear: {
         f: () => {
@@ -49,7 +48,7 @@ const commands = {
             self.term.scrollToTop();
             self.term.write("\x1b[2K\r>>> ");
         },
-        description: i18next?.t("help.clear")
+        description: () => i18next?.t("help.clear")
     },
     cls: {
         f: () => {
@@ -57,14 +56,14 @@ const commands = {
             self.term.scrollToTop();
             self.term.write("\x1b[2K\r>>> ");
         },
-        description: i18next?.t("help.cls")
+        description: () => i18next?.t("help.cls")
     },
     exit: {
         f: () => {
             window.localStorage.i18nextLng = "en";
             location.reload();
         },
-        description: i18next?.t("help.exit")
+        description: () => i18next?.t("help.exit")
     },
     gen: {
         f: () => {
@@ -80,7 +79,7 @@ const commands = {
                     } else {
                         self.term.write(`\n\x1b[31;1mTraceback (most recent call last):\n  File "<console>", line 1, in <module>\n\rNameError: name 'img' is not defined\x1b[m`);
                     }
-                    generation().catch(console.error); // Sometimes wrong colors when CI/CD
+                    generation().catch(console.error);
                 } else {
                     self.term.write(`\n\x1b[31;1mTraceback (most recent call last):\n  File "<console>", line 1, in <module>\n\rNameError: name 'svg' is not defined\x1b[m\r\n>>> `);
                 }
@@ -99,7 +98,6 @@ const commands = {
                         }
                         return value;
                     }
-
                     let [decompressed, pos] = ["", 0];
                     while (pos < data?.length) {
                         if (data?.charAt(pos) !== "`") {
@@ -121,14 +119,14 @@ const commands = {
                 QYCNkQmAmaE5gL31xYDRKfmAhe35gNmUyYCQ7TDJgI39aYChPXGAjaH5gK2FiYCI9XGAzT0hgKTI/YCRQKGAjcHRgM1NpYChRfmAybVRgJDZOYCRUPGAuJzJgJEI0YCNkfmA5SVpgLTt7YDw4T2Aje2dgKVQvYEE9JmAgM0pgI2J+YDFwfmAzInRgI2J+YDI8I2A3KVpgSG1+YDdKbWAgP35gIk9+YDZ4a2BNbnVgPEZcYD50amA9aWhgUX9DYDZlfmAiKn5gSDJ
                 0YEtuamAlS3VgNmV+YElsfmBILnNgIGZ+YFglYmA3MlFgRGxcYDZcfmBceX5gSylzYCJdfWBdZDNgI1x+YDE4emBhZX5gXFRVYCdJcGBiQ0BgRF9+YGJxbWBmUX5gYkluYE1MV2BnI0xgRG5pYEFNfmA/PX5gayBnYE1FVmAkOk1gWzB+YG9vfmBwd35gXmJ+YChVfmAoVX5gdSJ+YChVfmAoVX5gMi1+YDItfmB5YX5gMi1+YDJNXmBARH5gO2V+YHlhfmA7ZX5
                 gPDVOYEU9fmBudH5geWF+YF02fmBFYVpgTm9+YE5vfmBOb35geV5+YGcgVWBYPX5gWD1+YFg9fmBeYn5gcD9kYGFvfmBhb35gYW9+YGFvfmBmZHNga0V+YGtFfmBsZ35gbnd+YHR1fmB0dX5geWd+YHR1fmB0enlgeVp+YFtLbGBmZH5geVp+YDZlfmAoSX5gUVx+YHBVaGAoSX5gNmZ9YGthaGBqKX5gXmN+YC0+cmA2Z3xgcSBbYG50fmBxPn5gIXt+YDc5SmB
-                jU35geihUYHR1fmAoPm1gIj1pYHcyPWBwaD9gQ1R2YDJYaWB5YX5geXFHYCI8d2BwR2BgdC9pYHVeaWB5YX5geXFHYHYlaWBwKX5gfGR+YHlhfmAgZn5gcF59MG0=`.trim(); // XSS
+                jU35geihUYHR1fmAoPm1gIj1pYHcyPWBwaD9gQ1R2YDJYaWB5YX5geXFHYCI8d2BwR2BgdC9pYHVeaWB5YX5geXFHYHYlaWBwKX5gfGR+YHlhfmAgZn5gcF59MG0=`.trim(); // XSS sanat
                 const lz = decompressLZ77(window.atob(demoSVG))?.replace(/[.]/gm, "\x1b") || ""; // no URL here
                 if (lz) {
                     self.term.write(`\n${lz}\n\r>>> `);
                 }
             }
         },
-        description: i18next?.t("help.gen")
+        description: () => i18next?.t("help.gen")
     },
     help: {
         f: () => {
@@ -145,18 +143,18 @@ const commands = {
                 });
             }
         },
-        description: i18next?.t("help.lang")
+        description: () => i18next?.t("help.lang")
     },
     lipsum: {
         f: () => {
             const lorem = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                 "Logoden biniou degemer mat an penn ar bed Kernev en boan Kerbabu Groe erc'h, ennon botez plad koumanant outo hon leue ha Gerveur goañv miliner. Gar degouezhout du stagañ flourañ nav vrec'h aotre dor, vrozh werenn miz rimiañ oskaleg an hanternoz fest ur labourat urzh.",
                 "Pommy ipsum what a load of cobblers off the hook absolute twoddle scouser I'd reet fancy a, jolly have a butcher's at this the lakes it's me peepers warts and all, well chuffed football down the village green cor blimey' Northeners. A bottle of plonk as diamond geezer.",
-                "Taboire de verrat d'estique de patente à gosse de torrieux de maudine d'ostie de câline de bine de cibole de saint-sacrament de bâtard de rigodon de crisse de boswell de cossin de crucifix."/* lorembarnak */];
+                "Taboire de verrat d'estique de patente à gosse de torrieux de maudine d'ostie de câline de bine de cibole de saint-sacrament de bâtard de rigodon de crisse de boswell de cossin de crucifix."];
             const item = lorem[Math.floor(Math.random() * lorem.length)];
             self.term.write(`\n${item}\r\n>>> `);
         },
-        description: i18next?.t("help.lipsum")
+        description: () => i18next?.t("help.lipsum")
     },
     ls: {
         f: (command) => {
@@ -177,7 +175,7 @@ const commands = {
                 self.term.write(`\ndrwxr-xr-x 42 0 0 4096 ${date} .\ndrwxrwxrwx 42 0 0 4096 ${date} ..\n\r>>> `);
             }
         },
-        description: i18next?.t("help.ls")
+        description: () => i18next?.t("help.ls")
     },
     mode: {
         f: () => {
@@ -185,7 +183,7 @@ const commands = {
                 document.body.style.backgroundColor = "#FFF";
                 document.querySelector(".xterm-viewport").style.backgroundColor = "rgb(255, 255, 255)";
                 document.querySelector(".xterm-screen").style = "-webkit-filter: invert(100%); filter: invert(100%);";
-                self.isMode = true; // self.term._core._themeService.colors.background
+                self.isMode = true;
                 self.term.write("\n🎨\n\r>>> ");
             } else {
                 document.body.removeAttribute("style");
@@ -195,19 +193,19 @@ const commands = {
                 self.term.write("\n🖌️\n\r>>> ");
             }
         },
-        description: i18next?.t("help.mode")
+        description: () => i18next?.t("help.mode")
     },
     quit: {
         f: () => {
             location.reload();
         },
-        description: i18next?.t("help.quit")
+        description: () => i18next?.t("help.quit")
     },
     reboot: {
         f: () => {
             location.reload();
         },
-        description: i18next?.t("help.reboot")
+        description: () => i18next?.t("help.reboot")
     },
     version: {
         f: () => {
@@ -217,7 +215,7 @@ const commands = {
                 self.term.write('\nPython 3.11.2 (main, May  3 2023, 04:00:05) [Clang 17.0.0 (https://github.com/llvm/llvm-project df82394e7a2d06506718cafa347b on emscripten\nType "help", "copyright", "credits" or "license" for more information.\n\r>>> ');
             }
         },
-        description: i18next?.t("help.version")
+        description: () => i18next?.t("help.version")
     }
 };
 
